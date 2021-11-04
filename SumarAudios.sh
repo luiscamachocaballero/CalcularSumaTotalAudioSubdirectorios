147fd40857c9c8a3eb5a2eb9c31458b460c336fb
Luis Camacho <camacho.l@pucp.pe>
2021-11-04 09:33:52 -0500

    Update SumarAudios.sh

--- SumarAudios.sh
+++ SumarAudios.sh
@@ -2,3 +2,7 @@
 for file in $(find . -type f -print); do 
   ffprobe -v error -select_streams a:0 -show_entries stream=duration -of default=noprint_wrappers=1:nokey=1 "$file"
 done|paste -sd+|bc -l
+# en caso que el directorio no tenga subdirectorios, reemplazar la primera fila por
+#for file in *; do
+#en caso que el directorio sin subdirectorios tenga archivos de varios tipos, puede acotarse la iteración
+# usando *.wav o *.mp3 en lugar de *
